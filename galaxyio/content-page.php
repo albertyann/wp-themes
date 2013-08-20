47655202e6250e81ccfc7c91cf4541c8119eb1ef
yuntao <albertyann@gmail.om>
2013-08-20 12:08:47 +0800

    add aticle page span12

--- galaxyio/content-page.php
+++ galaxyio/content-page.php
@@ -8,16 +8,18 @@
  */
 ?>
 
-<h3><?php the_title(); ?></h3>
-<hr>
+<div class="span12">
+    <h3><?php the_title(); ?></h3>
+    <hr>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
-	</div><!-- .entry-content -->
-	
-	<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
-	
-</article><!-- #post-<?php the_ID(); ?> -->
+            <div class="entry-content">
+                    <?php the_content(); ?>
+                    <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
+            </div><!-- .entry-content -->
+
+            <?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
+
+    </article><!-- #post-<?php the_ID(); ?> -->
+</div>
